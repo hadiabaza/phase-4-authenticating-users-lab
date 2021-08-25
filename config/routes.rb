abeeb93fc56fa283cf6284dc1faa3c21375e2468
Hadi Abaza <Hadiabaza1@gmail.com>
2021-08-25 00:39:28 -0400

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  # how to login
+  post '/login', to: 'sessions#create'
+  # how to log out
+  delete '/logout', to: 'sessions#destroy'
+  # how to stay logged in on refresh
+  get '/me', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
